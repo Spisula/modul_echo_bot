e9ad62ffd8d867bcc72f9de92d0eb846be36ea9e
vitpr <vit.pravo@gmail.com>
2023-05-22 23:58:11 +1000

    added buttons

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,6 +1,8 @@
 from aiogram import Router, F
-from aiogram.types import Message
+from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
+                           ReplyKeyboardRemove)
 from lexicon.lexicon import LEXICON_RU
+from aiogram.filters import Text
 
 
 # Инициализируем роутер уровня модуля
@@ -8,6 +10,26 @@ router: Router = Router()
 # создаем фильтр на самом роутере, чтобы не прописывать в каждом хэндлере.
 # Этот фильтра пропускает только стикеры
 #router.message.filter(F.sticker)
+
+button_1: KeyboardButton = KeyboardButton(text='Собак 🦮')
+button_2: KeyboardButton = KeyboardButton(text='огурцов 🥒')
+
+keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+                                    keyboard=[[button_1, button_2]])
+
+@router.message(F.text == 'задай вопрос')
+async def question_button(msg: Message):
+    await msg.answer(text='чего кошки боятся больше всего', reply_markup=keyboard)
+
+@router.message(F.text == 'Собак 🦮')
+async def answer_dogs(msg: Message):
+    await msg.answer(text='Да, кошки боятся собак',
+                     reply_markup=ReplyKeyboardRemove)
+
+@router.message(F.text == 'огурцов 🥒')
+async def answer_cuc(msg: Message):
+    await msg.answer(text='Но огурцов кошки боятся еще больше',
+                     reply_markup = ReplyKeyboardRemove)
 
 @router.message(F.text)
 async def echo(msg: Message):
